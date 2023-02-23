7dbce731ac3ce8e814162271891fdad89c0c1cd0
mBaratta96 <marcobaratta96@gmail.com>
2023-02-23 19:43:41 +0100

    test metallum cli

--- src/main/java/metallum_cli/App.java
+++ src/main/java/metallum_cli/App.java
@@ -1,11 +1,22 @@
 package metallum_cli;
 
+import com.github.loki.afro.metallum.search.query.entity.BandQuery;
+import com.github.loki.afro.metallum.search.API;;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BandQuery query = BandQuery.byName("slayer", false);
+        API.getBandsFully(query).forEach(band -> {
+            System.out.println("Bandname: " + band.getName());
+            System.out.println("Bandgenre: " + band.getGenre());
+            System.out.println("Bandstatus: " + band.getStatus());
+            System.out.println("Partial Discs: " + band.getDiscsPartial());
+            System.out.println("Discs: " + band.getDiscs());
+            System.out.println("---");
+        });
     }
 }
