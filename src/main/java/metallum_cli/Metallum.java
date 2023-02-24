b310530cc769f711d16d8405551dd1d706347e1c
mBaratta96 <marcobaratta96@gmail.com>
2023-02-24 11:47:11 +0100

    get table

--- src/main/java/metallum_cli/Metallum.java
+++ src/main/java/metallum_cli/Metallum.java
@@ -2,7 +2,10 @@ package metallum_cli;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.net.http.HttpRequest;
 
 /**
  * Hello world!
@@ -12,13 +15,27 @@ public class Metallum {
     private static String metallum_url = "https://www.metal-archives.com/";
     private static String band_url = metallum_url.concat("bands/");
 
-    public static void main(String[] args) {
-        String test_url = band_url.concat("Panphage");
+    private String getWebpage(String band) throws IOException {
+        String test_url = band_url.concat(band);
         try {
             Document metallum_page = Jsoup.connect(test_url).get();
-            System.out.println(metallum_page.title());
+            Element discography = metallum_page.getElementById("band_disco");
+            Element complete_discography = discography.getElementsByTag("a").select(":contains(Complete)").first();
+            return complete_discography.attr("href");
         } catch (IOException e) {
-            System.out.println(e);
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        Metallum app = new Metallum();
+        try {
+            String discography_url = app.getWebpage("Panphage");
+            System.out.println(discography_url);
+            Document discography_table = Jsoup.connect(discography_url).get();
+            System.out.println(discography_table);
+        } catch (IOException e) {
+            System.err.println(e);
             return;
         }
     }
