afb056289de729afd9ede8adfa03344ee8f76040
mBaratta96 <marcobaratta96@gmail.com>
2023-02-26 02:01:15 +0100

    get rows

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module music-scrapper
 
 go 1.18
 
-require rsc.io/quote v1.5.2
+require github.com/gocolly/colly v1.2.0
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
@@ -10,16 +10,15 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.2.4 // indirect
 	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly/v2 v2.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
-	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
-	rsc.io/sampler v1.3.0 // indirect
 )
--- hello.go
+++ hello.go
@@ -2,9 +2,46 @@
 package main
 
 import "fmt"
-import "rsc.io/quote"
+import "github.com/gocolly/colly"
+import "strings"
 
+type table struct {
+    Name string
+    Type string
+    Year string
+    Review string
+}
 
 func main() {
-    fmt.Println(quote.Go())
+    c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("#band_disco a[href]", func(e *colly.HTMLElement) {
+        if strings.Contains(e.Attr("href"), "all") {
+            e.Request.Visit(e.Attr("href"))
+        }
+	})
+
+    c.OnHTML("table.display.discog tbody", func(h *colly.HTMLElement) {
+        rows := make([]table, 0)
+        h.ForEach("tr", func(i int, h *colly.HTMLElement) {
+            row := table{}
+            h.ForEach(".album,.demo,.other,td a[href]", func(i int, h *colly.HTMLElement) {
+                switch i {
+                    case 0:
+                        row.Name = h.Text
+                    case 1:
+                        row.Type = h.Text
+                    case 2:
+                        row.Year = h.Text
+                    case 3:
+                        row.Review = h.Text
+                }
+            })
+            rows = append(rows, row)
+        })
+        fmt.Println(rows)
+    })
+
+	c.Visit("https://www.metal-archives.com/bands/Panphage/")
 }
